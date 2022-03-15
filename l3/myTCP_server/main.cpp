634c3fd806b84594b116e23cd9a9d63b379626d6
gitMike000 <76992900+gitMike000@users.noreply.github.com>
2022-03-15 22:48:29 +0300

    Add files via upload

--- l3/myTCP_server/main.cpp
+++ l3/myTCP_server/main.cpp
@@ -34,14 +34,14 @@ int main(int argc, char const *argv[])
     const int port { std::stoi(argv[1]) };
 
     struct addrinfo hints, *addr;
-    memset(&hints,0,sizeof (hints));
-    hints.ai_family=AF_UNSPEC;
-    hints.ai_socktype=SOCK_STREAM;
-    hints.ai_flags=AI_PASSIVE;
-    hints.ai_protocol=IPPROTO_TCP;
-    getaddrinfo(NULL,argv[1],&hints,&addr);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET6;//AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_protocol = IPPROTO_TCP;
+    getaddrinfo(NULL, argv[1], &hints, &addr);
 
-    socket_wrapper::Socket sock = {addr->ai_family, addr->ai_socktype, addr->ai_protocol};
+    socket_wrapper::Socket sock = { addr->ai_family, addr->ai_socktype, addr->ai_protocol };
 
     std::cout << "Starting echo server on the port " << port << "...\n";
 
@@ -51,7 +51,7 @@ int main(int argc, char const *argv[])
         return EXIT_FAILURE;
     }
 
-    if  (bind(sock, addr->ai_addr, addr->ai_addrlen)!=0)
+    if  (bind(sock, addr->ai_addr, addr->ai_addrlen) != 0)
     {
         std::cerr << sock_wrap.get_last_error_string() << std::endl;
         // Socket will be closed in the Socket destructor.
@@ -63,18 +63,25 @@ int main(int argc, char const *argv[])
 
     std::cout << "Running echo server...\n" << std::endl;    
 
-    bool run=true;
+    bool run = true;
 
     listen(sock, 5);
 
-    int newsockfd;
+    int newsockfd = -1;
 
-    struct sockaddr_storage client_addr = {0};
+    struct sockaddr_storage client_addr = { 0 };
     socklen_t client_len = sizeof(client_addr);
     char ipbuff[INET_ADDRSTRLEN];
 
-    newsockfd = accept(sock, reinterpret_cast< sockaddr *>(&client_addr), &client_len);
+    while (newsockfd == -1)
+        newsockfd = accept(sock, reinterpret_cast< sockaddr *>(&client_addr), &client_len);
+
+    std::cout << "Connected" << std::endl;
+
+    // How him shutdowning?
+    shutdown(sock,2);
     close(sock);
+
     std::string str_buffer;
 
     while (run)
@@ -83,30 +90,32 @@ int main(int argc, char const *argv[])
         {
             throw std::runtime_error("ERROR on accept");
         }
-        char buffer[256]={0};
-        recv_len=recv(newsockfd, buffer, sizeof(buffer) - 1, 0);
+        char buffer[256] = { 0 };
+        recv_len = recv(newsockfd, buffer, sizeof(buffer) - 1, 0);
         if (recv_len > 0)
         {
             //buffer[recv_len] = '\0';
-            str_buffer=std::string(buffer);
+            str_buffer = std::string(buffer);
 
-            char hbuf[NI_MAXHOST]="\0";
+            char hbuf[NI_MAXHOST] = "\0";
             getnameinfo(reinterpret_cast<sockaddr *>(&client_addr), client_len, hbuf, sizeof(hbuf), nullptr, 0, NI_NAMEREQD);
 
             if (AF_INET == ((struct sockaddr_in *)&client_addr)->sin_family)
             {
                 inet_ntop(client_addr.ss_family, &(((struct sockaddr_in *)&client_addr)->sin_addr), ipbuff, INET_ADDRSTRLEN);
-            } else  if (AF_INET6 == ((struct sockaddr_in6 *)&client_addr)->sin6_family)
+            }
+            else  if (AF_INET6 == ((struct sockaddr_in6 *)&client_addr)->sin6_family)
             {
                 inet_ntop(client_addr.ss_family, &(((struct sockaddr_in6 *)&client_addr)->sin6_addr), ipbuff, INET_ADDRSTRLEN);
-            } else
+            }
+            else
             {
-                *ipbuff=client_addr.ss_family;
+                *ipbuff = client_addr.ss_family;
             }
             std::cout
                 << "Client with address "
                 << ipbuff
-                << "(name="<< hbuf<<")"
+                << "(name=" << hbuf<<")"
                 << ":" << ((struct sockaddr_in *)&client_addr)->sin_port
                 << " sent datagram "
                 << "[length = "
@@ -118,16 +127,16 @@ int main(int argc, char const *argv[])
 
             send(newsockfd, str_buffer.c_str(), str_buffer.length(), 0);
                //Exit
-            if (str_buffer.find("exit")==0)
+            if (str_buffer.find("exit") == 0)
             {
                 std::cout << "Breaking" << std::endl;
-                run=false;
+                run = false;
             };
         }
     }
 
     close(newsockfd);
+    freeaddrinfo(addr);
 
     return EXIT_SUCCESS;
 }
-
